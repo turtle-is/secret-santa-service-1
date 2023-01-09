e4162f24290fb70f45db4eddf6d5baecb716b071
mihyilo <miha170303@gmail.com>
2023-01-09 03:51:21 +0300

    create group.....

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -5,6 +5,7 @@ use std::{
 };
 
 use tide::Request;
+use crate::Access::Guest;
 
 #[derive(serde::Serialize, serde::Deserialize, Debug)]
 #[serde(rename_all = "snake_case")]
@@ -24,110 +25,141 @@ struct Group {
 }
 
 #[derive(serde::Serialize, serde::Deserialize)]
-struct User{
+struct User {
     name: String,
     access: Access,
     group: String,
-    recipient: String
+    recipient: String,
+}
+
+#[derive(serde::Deserialize)]
+struct AddGroup {
+    name: String,
+    creator: String,
 }
 
 #[derive(serde::Serialize, serde::Deserialize)]
-struct DataBase{
+struct DataBase {
     users: HashMap<String, User>,
-    groups: HashMap<String, Group>
+    groups: HashMap<String, Group>,
 }
 
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let version: &'static str = env!("CARGO_PKG_VERSION");
 
-fn main() -> Result<(), std::io::Error> {
-    let f = async {
-        let version: &'static str = env!("CARGO_PKG_VERSION");
+    let database = match File::open("data.base") {
+        Ok(file) => serde_json::from_reader(file).map_err(|err| {
+            let err = std::io::Error::from(err);
+            std::io::Error::new(
+                err.kind(),
+                format!("Failed to read from database file. {err}"),
+            )
+        })?,
+        Err(err) if err.kind() == std::io::ErrorKind::NotFound => {
+            eprintln!("Database file not found. Creating one.");
 
-        let database = match File::open("data.base") {
-            Ok(file) => serde_json::from_reader(file).map_err(|err| {
+            let file = File::create("data.base").map_err(|err| {
+                std::io::Error::new(err.kind(), format!("Failed to create database file. {err}"))
+            })?;
+
+            let database = DataBase {
+                users: HashMap::new(),
+                groups: HashMap::new(),
+            };
+
+            serde_json::to_writer(file, &database).map_err(|err| {
                 let err = std::io::Error::from(err);
                 std::io::Error::new(
                     err.kind(),
-                    format!("Failed to read from database file. {err}"),
+                    format!("Failed to write to database file. {err}"),
                 )
-            })?,
-            Err(err) if err.kind() == std::io::ErrorKind::NotFound => {
-                eprintln!("Database file not found. Creating one");
+            })?;
 
-                let file = File::create("data.base").map_err(|err| {
-                    std::io::Error::new(
-                        err.kind(),
-                        format!("Failed to create database file. {err}"),
-                    )
-                })?;
-                let database = DataBase {
-                    users: HashMap::new(),
-                    groups: HashMap::new()
-                };
-                serde_json::to_writer(file, &database).map_err(|err| {
-                    let err = std::io::Error::from(err);
-                    std::io::Error::new(
-                        err.kind(),
-                        format!("Failed to write to database file. {err}"),
-                    )
-                })?;
-
-                database
-            }
-            Err(err) => {
-                panic!("Failed to open database file. {err}");
-            }
-        };
-
-        let state = Arc::new(Mutex::new(database));
-
-        let mut app = tide::with_state(state);
-        app.at("/version")
-            .get(move |_| async move { Ok(serde_json::json!({ "version": version })) });
-
-        app.at("/")
-            .get(move |_| async move { Ok( "IT WORKS!")  });
-
-        app.at("/add-user")
-            .put(|mut request: Request<Arc<Mutex<DataBase>>>| async move {
-                let User { name, access, group:_, recipient:_ } = request.body_json().await?;
-
-                let body = request.body_string().await?;
-                eprintln!("Body: {}", body);
-
-                eprintln!("Adding user {name} with {access:?}");
-
-                let state = request.state();
-                let mut guard = state.lock().unwrap();
-
-                let name2 = name.clone();
-
-                guard.users.insert(name2, User{name, access, group: "".to_string(), recipient: "".to_string() });
-
-                Ok(tide::StatusCode::Ok)
-            });
-
-        app.at("/get-user")
-            .get(|mut request: Request<Arc<Mutex<DataBase>>>| async move {
-                let name: String = request.body_json().await?;
-
-                let state = request.state();
-                let guard = state.lock().unwrap();
-
-                eprintln!("Searching for user {name}");
-
-                match guard.users.get(&name){
-                    None => Err(tide::Error::from_str(
-                        tide::StatusCode::NotFound,
-                        format!("User {name} not found"),
-                    )),
-                    Some(access) => Ok(serde_json::json!({"access": access})),
-                    //Some(group) => Ok(serde_json::json!({"group": group})),
-                    //Some(recipient) => Ok(serde_json::json!({"recipient": recipient})),
-                }
-            });
-
-        app.listen("127.0.0.1:8080").await
+            database
+        }
+        Err(err) => {
+            panic!("Failed to open database file. {err}");
+        }
     };
 
-    futures::executor::block_on(f)
+    let state = Arc::new(Mutex::new(database));
+
+    let mut app = tide::with_state(state);
+    app.at("/version").get(move |_| async move { Ok(serde_json::json!({ "version": version })) });
+
+    app.at("/add-user")
+        .put(|mut request: Request<Arc<Mutex<DataBase>>>| async move {
+            let name: String = request.body_json().await?; // <--------------------- bruh
+
+            eprintln!("Adding user {name}");
+
+            let state = request.state();
+            let mut guard = state.lock().unwrap();
+
+            let name2 = name.clone();
+
+            guard.users.insert(name2, User { name, access: Guest, group: "".to_string(), recipient: "".to_string() });
+
+            Ok(tide::StatusCode::Ok)
+        });
+
+    app.at("/get-user")
+        .get(|mut request: Request<Arc<Mutex<DataBase>>>| async move {
+            let name: String = request.body_json().await?;
+
+            let state = request.state();
+            let guard = state.lock().unwrap();
+
+            eprintln!("Searching for user {name}");
+
+            match guard.users.get(&name) {
+                None => Err(tide::Error::from_str(
+                    tide::StatusCode::NotFound,
+                    format!("User {name} not found"),
+                )),
+                Some(user) => Ok(serde_json::json!({"access": user.access, "group": user.group, "recipient": user.recipient})),
+            }
+        });
+    app.at("/create-group")
+        .get(|mut request: Request<Arc<Mutex<DataBase>>>| async move {
+            let AddGroup { name, creator } = request.body_json().await?;
+
+            let state = request.state();
+            let guard = state.lock().unwrap();
+
+            eprintln!("Searching for group {name}");
+
+
+            match guard.users.get(&creator) {
+                None =>Err(tide::Error::from_str(
+                    tide::StatusCode::Conflict,
+                    format!("User {creator} not found or Group {name} already exist.")
+                )),
+                Some(user) => {
+                    match guard.groups.get(&name) {
+                        Some(..)=> Err(tide::Error::from_str(
+                            tide::StatusCode::Conflict,
+                            format!("User {creator} not found or Group {name} already exist."))),
+                        None => Ok({
+
+                            let group = Group {
+                                name: name.clone(),
+                                creator: creator.clone(),
+                                members: vec![creator.clone()],
+                                admins: vec![creator.clone()],
+                                closed: false,
+                            };
+
+                            guard.groups.insert(name.clone(), group);
+                            guard.users.get_mut(&creator).unwrap().group = name.clone();
+                        })
+                    }
+                }
+            }.unwrap();
+
+            Ok(tide::StatusCode::Ok)
+        });
+
+    app.listen("127.0.0.1:8080").await
 }
